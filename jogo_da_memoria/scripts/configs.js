ac103bd04ec2cdae642aa54f946107cceccf06c6
Leorna <42484610+Leorna@users.noreply.github.com>
2019-05-05 21:01:02 -0300

    Add files via upload

---
+++ jogo_da_memoria/scripts/configs.js
@@ -0,0 +1,147 @@
+'use strict'
+
+/*CONFIGURACOES DO JOGO*/
+
+let textosSobrepostos
+
+class ControladorDeAudio {
+    constructor() {
+        //audio quando o jogador perde
+        this.$perdeuAudio = new Audio('audios/Going-Different-Ways.mp3')
+
+        //audio quando o jogador ganha
+        this.$ganhouAudio = new Audio('audios/Cheering 3-SoundBible.com-1680253418.mp3')
+
+        //audio quando a carta vira
+        this.$flipAudio = new Audio('audios/Card-flip-sound-effect.mp3')
+
+        //audio quando há duas cartas iguais
+        this.$matchAudio = new Audio('audios/Strike-a-match.mp3')
+    }
+
+    somFlip() {
+        //audio do flip da carta
+        this.$flipAudio.play()
+    } 
+
+    somMatch() {
+        //audio de quando o jogador acerta duas cartas
+        this.$matchAudio.play()
+
+        //em 1 segundo pausa o audio
+        setTimeout(this.$matchAudio.pause, 1000)
+
+        //definindo tempo atual do audio como 0
+        this.$matchAudio.currentTime = 0
+    }
+
+    somGanhou() {
+        //audio de quando o jogador ganha
+        this.$ganhouAudio.play()
+
+        setTimeout(this.$ganhouAudio.pause, 3000)
+    }
+
+    somPerdeu() {
+        //audio de quando o jogador perde
+        this.$perdeuAudio.play()
+
+        setTimeout(()=> {
+            this.$perdeuAudio.pause()
+        }, 3000)
+    }
+}
+
+
+class ControladorDeJogo {
+    constructor(pontos, totalPontos, restartJogo, p) {
+        this.$tempoTotal = 50
+        this.$tempo = this.$tempoTotal
+        this.$timer = document.getElementById('tempo')
+        this.$audio = new ControladorDeAudio
+        this.$pontos = pontos
+        this.$totalPontos = totalPontos
+        this.restartJogo = restartJogo
+        this.$p = p
+    }
+
+    start() {
+        setTimeout(()=> {
+            this.contadorTempo = this.contarTempo()
+        }, 500)
+    }
+
+    contarTempo() {
+        return setInterval(()=> {
+            this.$tempo--
+            this.$timer.innerText = this.$tempo
+
+            if(this.$tempo === 0)
+                this.gameOver()
+            else if(this.conferirVitoria())
+                this.gameWin()
+
+        }, 1000)
+    }
+
+    addPontos() {
+        this.$pontos += 1
+        this.$p.innerHTML = pontos 
+    }
+
+    conferirVitoria() {
+        if(this.$pontos === this.$totalPontos)
+            return true
+        return false
+    }
+
+    gameOver() {
+        clearInterval(this.contadorTempo)
+        this.$timer.innerText = 50
+        this.$audio.somPerdeu()
+        document.getElementById('texto-game-over').classList.add('visivel')
+        this.restartJogo() 
+    }
+
+    gameWin() {
+        clearInterval(this.contadorTempo)
+        this.$timer.innerText = 50
+        this.$audio.somGanhou()
+        document.getElementById('texto-game-won').classList.add('visivel')
+        this.restartJogo()
+    }
+}
+
+
+function removerVisivel() {
+    this.classList.remove('visivel')
+    jogo.start() //objeto de jogablilidade.js
+}
+
+function loopPelosTextos() {
+    textosSobrepostos.forEach(textoSobreposto=> {
+        textoSobreposto.addEventListener('click', removerVisivel)
+    })
+}
+
+function iniciar() {
+    console.log('inicar()')
+    textosSobrepostos = document.querySelectorAll('.texto-sobreposto')
+
+    console.log(textosSobrepostos)
+
+    loopPelosTextos()
+}
+
+
+//se a pag estiver carregando
+if(document.readyState === 'loading') {
+    //DOMContentLoaded é acionado quando todo o HTML foi completamente carregado e analisado
+    document.addEventListener('DOMContentLoaded', iniciar)
+}
+//se a pag ja esta carregada
+else {
+    iniciar()
+}
+
+
---
+++ jogo_da_memoria/scripts/jogabilidade.js
@@ -0,0 +1,144 @@
+'use strict'
+
+/*JOGABILIDADE */
+
+
+/*querySelectorAll 
+retorna uma lista 
+de elementos 
+presentes no documento, uma lista com os elementos que tem a classe .carta*/ 
+const cartas = document.querySelectorAll('.carta') 
+console.log(cartas)
+
+const numeroDeCartas = cartas.length
+
+const p = document.getElementById('pontos')
+
+
+const totalPontos = 10
+let pontos = 0
+
+
+let possuiCartaGirada = false
+let bloquearJogoContent = false
+
+let primeiraCarta 
+let segundaCarta
+
+const audio = new ControladorDeAudio
+let jogo = new ControladorDeJogo(pontos, totalPontos, restartJogo, p)
+
+function restartJogo() {
+    cartas.forEach(carta=> {
+        carta.classList.remove('visivel')
+    })
+    pontos = 0
+    p.innerHTML = pontos
+
+    jogo = new ControladorDeJogo(pontos, totalPontos, restartJogo, p)
+    
+    main()
+}
+
+
+//remove os eventos das cartas que sao iguais
+function removerEventoDasCartas() {
+    primeiraCarta.removeEventListener('click', girarCarta)
+    segundaCarta.removeEventListener('click', girarCarta)
+
+    audio.somMatch()
+
+    //adiciona os pontos
+    jogo.addPontos()
+
+    //reseta primeira e segunda carta, e tambem possuiCartaGirada e bloquearJogoContent
+    resetJogoContent()
+}
+
+function desgirarCartas() {
+    bloquearJogoContent = true
+
+    setTimeout(()=> {
+        //remove a classe 'girar' das divs com classe .carta
+        primeiraCarta.classList.remove('visivel')
+        segundaCarta.classList.remove('visivel')
+
+        //redefine os valores de jogoContent
+        resetJogoContent()
+    }, 500)
+}
+
+function conferirIgualdade() {
+     //confere se as cartas sao iguais
+     let carta1 = primeiraCarta.dataset.iguais
+     let carta2 = segundaCarta.dataset.iguais
+
+     let iguais = carta1 === carta2
+
+     //se forem iguais removemos os eventos, se nao desgiramos as cartas
+     iguais? removerEventoDasCartas() : desgirarCartas()
+}
+
+function girarCarta() {
+    //se houver uma carta virada
+    if(bloquearJogoContent) return
+
+    //se a carta ja foi clicada, ou seja, é a primeira carta novamente
+    if(this === primeiraCarta) return
+
+    //this referencia um elemento com a classe carta
+    //classList.toggle adiciona se nao tiver e remove se tiver a classe girar a classe carta
+    //this.classList.toggle('girar')
+    this.classList.add('visivel') // add apenas adiciona a classe girar
+    //this é o elemento que disparou o evento
+
+    //audio de quando a carta é clicada
+    audio.somFlip()
+
+    if(!possuiCartaGirada) {
+        //primeira vez que o jogador clicou
+        possuiCartaGirada = true
+        primeiraCarta = this
+
+        return
+    }
+    
+    //segundo clique
+    possuiCartaGirada = false
+    segundaCarta = this
+
+    conferirIgualdade()
+    
+}
+
+function resetJogoContent() {
+    //[var1, var2] = [valor1, valor2] é o mesmo que
+    //let var1 = valor1
+    //let var2 = valor2
+
+    [possuiCartaGirada, bloquearJogoContent] = [false, false]
+    [primeiraCarta, segundaCarta] = [undefined, undefined]
+}
+
+// uma immediately invoked function
+//embaralha as cartas, mudando o valor default de order 
+(function embaralhar() {
+    const rand = Math.random
+    
+    //para cada item da lista de cartas, muda o style.order para um numero aleatorio de 0 a 19
+    cartas.forEach(carta=> {
+        let posicao = ~~(rand() * numeroDeCartas)
+        carta.style.order = posicao
+    })
+    console.log('embaralhar()')
+})()
+
+
+//main, para cada carta dentro da lista de cartas
+function main() {
+    cartas.forEach(carta=> {
+        carta.addEventListener('click', girarCarta)
+    })
+}
+
+main()
